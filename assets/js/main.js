53efd0dbbe5aedeb13c609f60282d939cfcbbaf6
MarionFoto <marionacp@gmail.com>
2017-04-26 09:33:03 -0300

    cargando cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
-function a(){
-
-	document.getElementsByClassName("class");
-
-}
- alert("Hola Mundo!");
+function mensaje(){
+			var msje = document.getElementsByClassName('mensaje');
+			if(msje.length == 1){
+				alert("Es muy poco");
+			}
+			else if(msje.length > 2 && msje.length < 4){
+				alert("No está mal");
+			} else{
+				alert("Son muchos!");
+			}
+		};
+		mensaje();
